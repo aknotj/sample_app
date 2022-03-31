65a6066552dfcac70bee754837d70e67a5ccc13e
aknotj <100099666+aknotj@users.noreply.github.com>
2022-03-31 23:10:59 +0000

    [Start]sample_app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "17454ca5613f46dabb4cbf3305bb2180.vfs.cloud9.us-east-1.amazonaws.com"
 end
